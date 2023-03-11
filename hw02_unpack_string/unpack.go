c57646327998ca8876742a54faa4550a7bb3210e
Michael Savin <jtprogru@gmail.com>
2023-03-11 12:29:11 +0300

    Hw02 unpack string

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,28 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	result := ""
+	for index := range input {
+		if input[index] >= 48 && input[index] <= 57 {
+			if index-1 < 0 || input[index-1] >= 48 && input[index-1] <= 57 {
+				return "", ErrInvalidString
+			}
+			count := int(input[index] - '0')
+			if count > 0 {
+				count = count - 1
+				result = result + strings.Repeat(string(input[index-1]), count)
+			} else if count == 0 {
+				result = string(input[:index-1])
+			}
+		} else {
+			result = result + string(input[index])
+		}
+	}
+	return result, nil
 }
